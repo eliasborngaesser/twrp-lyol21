71d014d5c89c498fb93fd4f9f20f1c03f821b20d
EliasBorngaesser <elias.borngaesser@gmail.com>
2022-05-30 16:59:24 +0200

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lyol21.mk
+
+COMMON_LUNCH_CHOICES := \
+    lyol21-user \
+    lyol21-userdebug \
+    lyol21-eng
